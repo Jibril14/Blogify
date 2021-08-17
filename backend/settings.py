0a8e72a4f82f8fa393cebce87dadd416c917070d
Abdullahi Abdulwasiu <Abdullahiabdulwasiu1@gmail.com>
2021-08-18 00:14:34 +0100

    jjj

--- backend/settings.py
+++ backend/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')],
+        'DIRS': [os.path.join(BASE_DIR, 'backend/frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
